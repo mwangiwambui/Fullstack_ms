a1239f9e54412e6773ed92870b6e183bac8c45ae
mwangiwambui <wambui54mwangi@gmail.com>
2021-06-17 07:39:28 +0300

    Added the get endpoints and the controller

---
+++ src/main/java/com/wambui/demo/student/Student.java
@@ -0,0 +1,44 @@
+package com.wambui.demo.student;
+
+import java.util.UUID;
+
+public class Student {
+    private final UUID studentId;
+    private final String firstName;
+    private final String lastname;
+    private final String email;
+    private final Gender gender;
+
+    public Student(UUID studentId, String firstName, String lastname, String email, Gender gender) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UUID getStudentId() {
+        return studentId;
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+
+}
