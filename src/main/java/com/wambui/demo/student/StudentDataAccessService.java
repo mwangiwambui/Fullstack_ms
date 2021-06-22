0e4f52d723fdf51c9913bc85e2fb091d86a23cbd
mwangiwambui <wambui54mwangi@gmail.com>
2021-06-22 21:53:15 +0300

    Created the respositories and added data into the database

---
+++ src/main/java/com/wambui/demo/student/StudentDataAccessService.java
@@ -0,0 +1,41 @@
+package com.wambui.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
+
+@Repository
+public class StudentDataAccessService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public StudentDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    List<Student> selectAllStudents() {
+        String sql = "" + "SELECT student_id, " + "first_name, " + "last_name, " + "email, " + "gender " + "FROM student";
+        return jdbcTemplate.query(sql, mapStudentFromDB());
+
+    }
+
+    private RowMapper<Student> mapStudentFromDB() {
+        return (resultSet, i) -> {
+            String studentIdStr = resultSet.getString("student_id");
+            UUID studentId = UUID.fromString(studentIdStr);
+
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String email = resultSet.getString("email");
+            String genderStr = resultSet.getString("gender").toUpperCase();
+            Student.Gender gender = Student.Gender.valueOf(genderStr);
+            return new Student(studentId, firstName, lastName, email, gender);
+        };
+    }
+}
---
+++ src/main/java/com/wambui/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.wambui.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+@Service
+public class StudentService {
+    private final StudentDataAccessService studentDataAccessService;
+
+    @Autowired
+    public StudentService(StudentDataAccessService studentDataAccessService) {
+        this.studentDataAccessService = studentDataAccessService;
+    }
+
+    public List<Student> getAllStudents(){
+        return studentDataAccessService.selectAllStudents();
+    }
+}
