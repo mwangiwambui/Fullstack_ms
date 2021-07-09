467b7187e7c9d108893bac404eb4858df8dc6f71
mwangiwambui <wambui54mwangi@gmail.com>
2021-07-09 17:53:28 +0300

    Submitting the form

--- src/js/src/forms/AddStudentForm.js
+++ src/js/src/forms/AddStudentForm.js
@@ -19,7 +19,7 @@ class AddStudentForm extends Component {
               }
 
           if (!values.email) {
-            errors.email = 'Required';
+            errors.email = 'Email Required';
           } else if (
             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
           ) {
@@ -48,6 +48,8 @@ class AddStudentForm extends Component {
           handleBlur,
           handleSubmit,
           isSubmitting,
+          submitForm,
+          isValid
           /* and other goodies */
         }) => (
           <form onSubmit={handleSubmit}>
@@ -91,7 +93,9 @@ class AddStudentForm extends Component {
             />
             {errors.gender && touched.gender && <Tag style= {tagStyle}>errors.gender</Tag>}
 
-            <Button type="submit" disabled={isSubmitting}>
+            <Button 
+            onClick={() => submitForm()} type="submit" 
+            disabled={isSubmitting | (touched && !isValid)}>
               Submit
             </Button>
           </form>
